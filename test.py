60f2ce5fc2cd6cbda947b437b9615ce780596042
harishwarj <harishwar.ds.ai@gmail.com>
2023-03-28 10:19:27 +0530

    Vulgar Python

---
+++ test.py
@@ -0,0 +1,9 @@
+name = input("Enter Your name : ")
+name = name.strip().lower()
+
+if (name == "hemanth"):
+    print("GAYY")
+elif name == "hari":
+    print("Trans Prostitute")
+else:
+    print("Hello World!!")
